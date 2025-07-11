9dbf7493ca7832db8a4199c981c94c57860f248b
Laurens Brosens <L.Brosens@chipsoft.com>
2025-07-11 16:27:32 +0200

    Added project

---
+++ Bots/LaurensBot/LaurensBot.cs
@@ -0,0 +1,17 @@
+﻿using NasdaqTrader.Bot.Core;
+
+namespace LaurensBot;
+public class LaurensTrader : ITraderBot
+{
+    public string CompanyName => "Laurens Inc. Investments";
+
+    public async Task DoTurn(ITraderSystemContext systemContext)
+    {
+        var listings = systemContext.GetListings();
+        var cash = systemContext.GetCurrentCash(this);
+        var currentDate = systemContext.CurrentDate;
+        var tradesLeft = systemContext.GetTradesLeftForToday(this);
+
+        systemContext.BuyStock(this, listings[0], 1);
+    }
+}
