a9d7c5c119e55d59fd592ea9cf28e94cd725e814
Laurens Brosens <L.Brosens@chipsoft.com>
2025-08-04 08:49:19 +0200

    wip

--- Bots/LaurensBot/LaurensBot.cs
+++ Bots/LaurensBot/LaurensBot.cs
@@ -3,8 +3,115 @@ using System.Collections.Concurrent;
 
 namespace LaurensBot;
 
+public class TradeAction
+{
+    public DateOnly StartDate { get; set; }
+    public DateOnly EndDate { get; set; }
+    public decimal PercentageIncrease { get; set; }
+    public decimal StartPrice { get; set; }
+    public decimal EndPrice { get; set; }
+}
+
 public class LaurensTrader : ITraderBot
 {
+    public string CompanyName => "Laurens Inc. Investments";
+    private bool _initial = true;
+    private ITraderSystemContext _systemContext = null!;
+    private ConcurrentBag<TradeAction> _tradeActions = [];
+    private ConcurrentBag<TradeAction> _activeTrades = [];
+
+
+    public async Task DoTurn(ITraderSystemContext systemContext)
+    {
+        _systemContext = systemContext;
+        if (_initial)
+        {
+            _initial = false;
+            await InitialCalculations();
+        }
+        _tradeActions.Where(a => a.StartDate == _systemContext.CurrentDate).OrderBy(a => a.PercentageIncrease);
+        // Loop door alle _activeTrades en verkoop diegene met een einddatum van vandaag
+        // Loop door alle _tradeActions voor vandaag en koop diegene 1. als er nog handelingen gedaan kunnen worden vandaag 2. amount groot genoeg is 3. einddatum nog niet vol zit
+
+        await Task.CompletedTask;
+    }
+
+    private Task InitialCalculations()
+    {
+        // De bruteforce manier om dit te doen zou zijn loopen over alle dagen en hoogste stijgingen berekenen voor elke startdag met 1 dag in de toekomst of 2 etc.
+
+        // Om alles veel sneller te maken zou ik een threshold kunnen instellen waarbij een bepaalde start/einddag geskipped wordt als er voldoende trades zijn en de stijging hoog genoeg is?
+
+        var startDate = _systemContext.StartDate;
+        var endDate = _systemContext.EndDate;
+        var listings = _systemContext.GetListings().ToList();
+        var dates = listings.First().PricePoints.Select(p => p.Date).ToArray();
+
+        try
+        {
+            foreach (var listing in listings)
+            {
+                var pricePoints = listing.PricePoints;
+
+                for (int i = 0; i < listing.PricePoints.Length; i++)
+                {
+                    for (int j = i + 1; j < listings.Count; j++)
+                    {
+                        var startPricePoint = listing.PricePoints[i];
+                        var endPricePoint = listing.PricePoints[j];
+                        var increase = CalculateIncrease(startPricePoint.Price, endPricePoint.Price);
+                        _tradeActions.Add(
+                            new TradeAction()
+                            {
+                                StartDate = startPricePoint.Date,
+                                EndDate = endPricePoint.Date,
+                                StartPrice = startPricePoint.Price,
+                                EndPrice = endPricePoint.Price,
+                                PercentageIncrease = increase
+                            }
+                        );
+                    }
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            throw;
+        }
+
+        return Task.CompletedTask;
+    }
+
+    private decimal CalculateIncrease(decimal a, decimal b)
+    {
+        if (a == 0)
+        {
+            return -1;
+        }
+        return ((b - a) / a);
+    }
+    /* Doesn't work because pricepoints aren't 365 days
+    private DateOnly[] AllDates(DateOnly startDate, DateOnly endDate)
+    {
+        List<DateOnly> dates = [];
+        DateOnly currentDate = startDate;
+        while (currentDate <= endDate)
+        {
+            dates.Add(currentDate);
+            currentDate = currentDate.AddDays(1);
+        }
+        return dates.ToArray();
+    }
+    */
+    // Idee: bereken alles upfront en ga daarna pas 1 voor 1 door alle trades obv. wat berekend is geraakt
+    // Simpelweg de best mogelijk trades berekenen met 1 startdatum en 1 einddatum
+    // Als de trades voor de startdatums volzitten skip deze dan, als de trades voor de einddatum volzitten skip de trade
+    // Sorteer op prijsincrease (ongeacht datum), eigenlijk zou er een multiplier moeten zijn om grote increases op lange termijn een voordeel te geven?
+
+
+
+
+    /* Legacy code
     public string CompanyName => "Laurens Inc. Investments";
     private decimal _currentHighest = 0;
     private IStockListing? _currentHighestListing = null;
@@ -35,12 +142,11 @@ public class LaurensTrader : ITraderBot
 
     public async Task DoTurn(ITraderSystemContext systemContext)
     {
-        /*
-        Console.WriteLine();
-        Console.WriteLine();
-        Console.WriteLine($"A new day");
-        Console.WriteLine();
-        */
+        //Console.WriteLine();
+        //Console.WriteLine();
+        //Console.WriteLine($"A new day");
+        //Console.WriteLine();
+        
         // Problem: je kan slechts 5 transacties doen, je zou kunnen zeggen dat je 3 koopt 2 verkoopt, maar dan zit je geld vast in random aandelen, Als je 2 koopt en 3 verkoopt doe je maar 4
         // transacties wat niet optimaal is. Dus je kan om de dag switchen: 3 kopen 2 verkopen, 2 kopen 3 verkopen etc. Dit is vrij gemakkelijk: verkoop alles en switch tussen 2 en 3 kopen.
         // Ipv. de 3 meest stijgende aandelen, koop je de 3 meest stijgende waarmee het meeste geld weg is (profit is hoger in dat geval!).
@@ -155,29 +261,29 @@ public class LaurensTrader : ITraderBot
 
     private void SellAll(ITraderSystemContext systemContext)
     {
-        /*
-        List<(IStockListing listing, int amount)> stocksToSell = [];
-        foreach (var holding in systemContext.GetHoldings(this))
-        {
-            if (holding.Amount > 0)
-            {
-                stocksToSell.Add((holding.Listing, holding.Amount));
-            }
-        } // Sell all if needed.
+        
+        //List<(IStockListing listing, int amount)> stocksToSell = [];
+        //foreach (var holding in systemContext.GetHoldings(this))
+        //{
+        //    if (holding.Amount > 0)
+        //    {
+        //        stocksToSell.Add((holding.Listing, holding.Amount));
+        //    }
+        //} // Sell all if needed.
 
-        if (!_start)
-        {
-            var currentCash = systemContext.GetCurrentCash(this);
-            _start = currentCash > 100000;
-        }
-        else
-        {
-            stocksToSell = stocksToSell.Where(h => !_longBuys.Contains(h.listing)).ToList(); // Start ignoring longs now
-        }*/
+        //if (!_start)
+        //{
+        //    var currentCash = systemContext.GetCurrentCash(this);
+        //    _start = currentCash > 100000;
+        //}
+        //else
+        //{
+        //    stocksToSell = stocksToSell.Where(h => !_longBuys.Contains(h.listing)).ToList(); // Start ignoring longs now
+        //}
 
         foreach (var (listing, amount) in _boughtYesterday)
         {
             systemContext.SellStock(this, listing, amount);
         }
-    }
+    }*/
 }
